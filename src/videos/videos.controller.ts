79e285f8fa8e7f6d47959c9bd7884aede4c85f17
Binh Minh <minhngoc2891211@gmail.com>
2024-10-18 15:36:37 +0700

    fix video fastforwarding feature

--- src/videos/videos.controller.ts
+++ src/videos/videos.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Param, Res } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  Req,
+  Res,
+  StreamableFile,
+} from '@nestjs/common';
 import { Response } from 'express';
 import { VideosService } from './videos.service';
 import * as fs from 'fs';
@@ -32,20 +40,46 @@ export class VideosController {
       },
     },
   })
-  Video(@Param('filename') filename: string, @Res() res: Response) {
+  Video(
+    @Param('filename') filename: string,
+    @Res() res: Response,
+    @Req() req: Request,
+  ) {
     const videoPath = this.videosService.getVideo(filename);
-    const stat = fs.statSync(videoPath);
-    const fileSize = stat.size;
-    const stream = fs.createReadStream(videoPath);
-    res.writeHead(200, {
-      'Content-Length': fileSize,
-      'Content-Type': 'video/mp4',
-    });
-    stream.pipe(res);
-    stream.on('error', (err) => {
-      res.sendStatus(500);
-      console.error(err);
-    });
+    const videoStat = fs.statSync(videoPath);
+    const fileSize = videoStat.size;
+
+    // Check if the Range header is present
+    const range = req.headers['range'] as string | undefined;
+    if (range) {
+      // Extract start and end from the Range header
+      const parts = range.replace(/bytes=/, '').split('-');
+      const start = parseInt(parts[0], 10);
+      const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+
+      // Create a partial response (206)
+      const chunkSize = end - start + 1;
+      const videoStream = fs.createReadStream(videoPath, { start, end });
+
+      res.set({
+        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': chunkSize,
+        'Content-Type': 'video/mp4',
+      });
+
+      res.status(HttpStatus.PARTIAL_CONTENT); // 206 status code for partial content
+      videoStream.pipe(res);
+    } else {
+      // No range request, send the whole video
+      res.set({
+        'Content-Length': fileSize,
+        'Content-Type': 'video/mp4',
+      });
+
+      const videoStream = fs.createReadStream(videoPath);
+      videoStream.pipe(res);
+    }
   }
 
   @ApiResponse({
